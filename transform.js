69ec11748410a36791c7bfc48063128977fb7538
Eric Le Lay <elelay@macports.org>
2016-08-30 16:31:19 +0200

    real serialize ; updater

--- transform.js
+++ transform.js
@@ -2,10 +2,11 @@
 
 const fs = require("fs");
 const request = require("request");
-
+const server = "http://kinto.elelay.fr";
+const bucket = "jedit-trackers";
 //request.debug = true;
 
-const usage = "Usage: " + process.argv[0] + " " + process.argv[1] + " <JEDIT_EXPORT_DIR> <RESULT>";
+const usage = "Usage: " + process.argv[0] + " " + process.argv[1] + " <JEDIT_EXPORT_DIR> <TOKEN>";
 
 if (process.argv.length != 4 || process.argv[2].match(/--?h(elp)?/)) {
     console.log(usage);
@@ -13,7 +14,7 @@ if (process.argv.length != 4 || process.argv[2].match(/--?h(elp)?/)) {
 }
 
 const exportDir = process.argv[2];
-const resultFile = process.argv[3];
+const token = process.argv[3];
 
 function load(file) {
     file = exportDir + "/" + file;
@@ -63,33 +64,37 @@ function transformAndSave(objs) {
     var res = {
         "defaults": {
             "method": "POST",
-            "path": "/buckets/default/collections/tickets/records",
+            "path": "/buckets/" + bucket + "/collections/tickets/records",
         }
     };
     res.requests = inserts;
     return save(res);
 }
 
-function split(arr, test){
-	return arr.reduce(function(acc, item){
-		(test(item) ? acc[0] : acc[1]).push(item);
-		return acc;
-	}, [[],[]]);
+function split(arr, test) {
+    return arr.reduce(function(acc, item) {
+        (test(item) ? acc[0] : acc[1]).push(item);
+        return acc;
+    }, [
+        [],
+        []
+    ]);
 }
 
 var stats = {};
 var statsCnt = 0;
 var statsTotal = 0;
+
 function transform(objs) {
     var inserts = [];
     //var ticketIds = {};
     objs/*.slice(0,1)*/.forEach(function(obj) {
         var tickets = transformOne(obj);
-        var splitTickets = split(tickets, function(ticket){
-        	const open = (ticket.status.indexOf("open") === 0) || (ticket.status.indexOf("pending") === 0);
-        	const thisYear = (ticket.created_date.indexOf("2016") === 0)
-        		|| (ticket.mod_date.indexOf("2016") === 0);
-        	return open || thisYear;
+        var splitTickets = split(tickets, function(ticket) {
+            const open = (ticket.status.indexOf("open") === 0) || (ticket.status.indexOf("pending") === 0);
+            const thisYear = (ticket.created_date.indexOf("2016") === 0) ||
+                (ticket.mod_date.indexOf("2016") === 0);
+            return open || thisYear;
         });
         //for(var j=0;j<tickets.length;j++){
         //	if(ticketIds[tickets[j].id]){
@@ -98,52 +103,52 @@ function transform(objs) {
         //		ticketIds[tickets[j].id] = tickets[j].tracker_id;
         //	}
         //}
-        
+
         const assign = {
-        	tickets: splitTickets[0],
-        	oldTickets: splitTickets[1]
+            tickets: splitTickets[0],
+            oldTickets: splitTickets[1]
         };
-        
+
         //tickets = tickets.slice(0,400);
-        
-        statsTotal+=tickets.length;
-        
-        Object.keys(assign).forEach(function(collection){
-        	var tickets = assign[collection];
-        	if(tickets.length){
-				const tracker = tickets[0] && tickets[0].tracker_label;
-				const total = Math.ceil(tickets.length / 200);
-				stats[tracker] = [];
-				for (var i = 0; i < tickets.length; i += 200) {
-					var toSend = tickets.slice(i, Math.min(i + 200, tickets.length));
-					inserts.push(put(i/200, ((i/200)+1) + "/" + total + "("+collection+")", collection, toSend));
-					stats[tracker].push("-");
-				}
-			}
-		});
+
+        statsTotal += tickets.length;
+
+        Object.keys(assign).forEach(function(collection) {
+            var tickets = assign[collection];
+            if (tickets.length) {
+                const tracker = tickets[0] && tickets[0].tracker_label;
+                const total = Math.ceil(tickets.length / 200);
+                stats[tracker] = [];
+                for (var i = 0; i < tickets.length; i += 200) {
+                    var toSend = tickets.slice(i, Math.min(i + 200, tickets.length));
+                    inserts.push(put(i / 200, ((i / 200) + 1) + "/" + total + "(" + collection + ")", collection, toSend));
+                    stats[tracker].push("-");
+                }
+            }
+        });
     });
-    console.log("THERE ARE", statsTotal,"TICKETS");
-    setInterval(function(){
-    		console.log("=================================");
-    		console.log(statsCnt+"/"+statsTotal, "transferred");
-    		Object.keys(stats).forEach(function(tracker){
-    			console.log(tracker, stats[tracker].join(""));
-    		});
-    		console.log("=================================");
-    		if(statsCnt === statsTotal){
-    			process.exit(0);
-    		}
+    console.log("THERE ARE", statsTotal, "TICKETS");
+    setInterval(function() {
+        console.log("=================================");
+        console.log(statsCnt + "/" + statsTotal, "transferred");
+        Object.keys(stats).forEach(function(tracker) {
+            console.log(tracker, stats[tracker].join(""));
+        });
+        console.log("=================================");
+        if (statsCnt === statsTotal) {
+            process.exit(0);
+        }
     }, 5000);
 
     //return Promise.resolve("OK");
-    return Promise.all(inserts);
-    //return serialize(inserts);
+    //return Promise.all(inserts);
+    return serialize(inserts);
 }
 
 function serialize(promises) {
     if (promises.length) {
         var promise = promises.shift();
-        return promise.then(function() {
+        return promise().then(function() {
             serialize(promises);
         });
     } else {
@@ -156,7 +161,7 @@ function put(i, label, collection, tickets) {
         var batch = {
             "defaults": {
                 "method": "POST",
-                "path": "/buckets/default/collections/"+collection+"/records",
+                "path": "/buckets/" + bucket + "/collections/" + collection + "/records",
             }
         };
         batch.requests = tickets.map(function(ticket) {
@@ -166,44 +171,46 @@ function put(i, label, collection, tickets) {
                 }
             };
         });
-        return new Promise(function(resolve, reject) {
-            console.log("D: batch posting", tickets.length, "tickets for", tickets[0].tracker_label, label);
-            var req = {
-                method: "POST",
-                uri: "http://localhost:8888/v1/batch",
-                auth: {
-                    user: "token",
-                    pass: "tr0ub4d@ur"
-                },
-                body: batch,
-                json: true
-            };
-            //console.log(JSON.stringify(batch));
-            request(req, function(err, res, body) {
-                if (err) {
-                    reject(err);
-                } else {
-                	const tracker = tickets[0].tracker_label;
-                	console.log("D: status posting", tickets.length, "tickets for", tracker, label, "=", res.statusCode);
-                	statsCnt+=tickets.length;
-                    if (res.statusCode !== 200) {
-                        console.log("E: sending tickets, status=" + res.statusCode + ", body=" + JSON.stringify(body));
-                        stats[tracker][i] = "!";
-                        reject(new Error("E: sending tickets, status=" + res.statusCode + ", body=" + JSON.stringify(body)));
+        return function() {
+            return new Promise(function(resolve, reject) {
+                console.log("D: batch posting", tickets.length, "tickets for", tickets[0].tracker_label, label);
+                var req = {
+                    method: "POST",
+                    uri: server + "/v1/batch",
+                    auth: {
+                        user: "token",
+                        pass: token
+                    },
+                    body: batch,
+                    json: true
+                };
+                //console.log(JSON.stringify(batch));
+                request(req, function(err, res, body) {
+                    if (err) {
+                        reject(err);
                     } else {
-                        body.responses.forEach(function(rep) {
-                            if (rep.status != 200 && rep.status != 201) {
-                                console.error("E: sub req", rep);
-                                stats[tracker][i] = "!";
-                                reject(rep);
-                            }
-                        });
-                        stats[tracker][i] = "+";
-                        resolve(res);
+                        const tracker = tickets[0].tracker_label;
+                        console.log("D: status posting", tickets.length, "tickets for", tracker, label, "=", res.statusCode);
+                        statsCnt += tickets.length;
+                        if (res.statusCode !== 200) {
+                            console.log("E: sending tickets, status=" + res.statusCode + ", body=" + JSON.stringify(body));
+                            stats[tracker][i] = "!";
+                            reject(new Error("E: sending tickets, status=" + res.statusCode + ", body=" + JSON.stringify(body)));
+                        } else {
+                            body.responses.forEach(function(rep) {
+                                if (rep.status != 200 && rep.status != 201) {
+                                    console.error("E: sub req", rep);
+                                    stats[tracker][i] = "!";
+                                    reject(rep);
+                                }
+                            });
+                            stats[tracker][i] = "+";
+                            resolve(res);
+                        }
                     }
-                }
+                });
             });
-        });
+        };
     }
 }
 
---
+++ updater.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+import datetime
+import argparse
+
+import requests
+
+import kinto_http
+from kinto_http import Client
+
+parser = argparse.ArgumentParser(description='jEdit trackers Kinto DB updater.')
+parser.add_argument('-s', '--server', default="http://localhost:8888",
+                   help='kinto server (without the /v1)')
+parser.add_argument('-t', '--token', required=True,
+                   help='Authentication token')
+
+args = parser.parse_args()
+
+server = args.server;
+token = args.token;
+bucket = "jedit-trackers";
+
+print("Updating %s/v1/buckets/%s" % (server, bucket))
+
+client = Client(server_url= server + "/v1",
+                auth=('token', token))
+
+rt = requests.get("https://sourceforge.net/rest/p/jedit/")
+jsont = rt.json()
+trackers = [tool for tool in jsont['tools'] if tool['name'] == 'tickets']
+
+last_updater = client.create_record({'id':"last", 'last_date':"2016-08-11T00:00:00Z"}, bucket=bucket, collection="updater", if_not_exists=True)
+last_date = last_updater['data']['last_date']
+print("last_date: %s" % last_date)
+
+now_date = datetime.datetime.utcnow().replace(microsecond=0).isoformat() + "Z"
+
+print("now: %s" % now_date)
+for tool in trackers:
+    t = tool['mount_point']
+#    url = "https://sourceforge.net/rest/p/jedit/%s/?orderBy=mod_date_dt&order=desc&limit=10" % t
+    url = "https://sourceforge.net/rest/p/jedit/%s/search?q=mod_date_dt:[%s TO %s]" % (t, last_date, now_date)
+    print("Fetching %s..." % url)
+    r = requests.get(url)
+    print("%s: status %i" % (t, r.status_code))
+    if r.status_code is 200:
+        json = r.json()
+        print("%s: found %i tickets" % (t, json["count"]))
+        for ticket in json['tickets']:
+            ticket['id'] = ticket['_id']
+            del ticket['_id']
+            ticket['tracker_id'] = t
+            ticket['tracker_label'] = tool['mount_label']
+            rd = requests.get(ticket['discussion_thread_url'])
+            if rd.status_code is 200:
+                jsont = rd.json()
+                ticket["discussion_thread"] = jsont['thread']
+        
+        print("%s: updating %i tickets" % (t, len(json['tickets'])))
+        with client.batch() as batch:
+            for ticket in json['tickets']:
+                batch.update_record(ticket, bucket=bucket, collection="tickets")
+
+last_updater['data']['last_date'] = now_date
+# comment to disable update while testing
+client.update_record(last_updater['data'], collection='updater')
+print("DONE")
